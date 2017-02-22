520bdf8d60035a831e1054ce7e5910395b878b20
AlexDel <verbalab@yandex.ru>
2017-02-22 14:51:58 +0700

    analyzer ready

---
+++ classifier.py
@@ -0,0 +1,32 @@
+import csv
+import codecs
+
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+clf = MultinomialNB()
+
+from analizer import normalize
+vectorizer = TfidfVectorizer(analyzer=normalize)
+
+samples = []
+with codecs.open('./ready-data.tsv', 'r') as tsvFile:
+  tsvReader = csv.DictReader(tsvFile, delimiter = '\t')
+  for row in tsvReader:
+    samples.append({'text': row['INPUT:text'], 'tag': row['OUTPUT:emotion']})
+
+trainSet = vectorizer.fit_transform([t['text'] for t in samples])
+
+clf.fit(trainSet, [t['tag'] for t in samples])
+
+
+def most_informative_feature_for_class(vectorizer, classifier, classlabel, n=10):
+  labelid = list(classifier.classes_).index(classlabel)
+  feature_names = vectorizer.get_feature_names()
+  topn = sorted(zip(classifier.feature_log_prob_[labelid], feature_names))[-n:]
+
+  for coef, feat in topn:
+    print(classlabel, feat, coef)
+
+for emotion in set([t['tag'] for t in samples]):
+  most_informative_feature_for_class(vectorizer, clf, emotion)
