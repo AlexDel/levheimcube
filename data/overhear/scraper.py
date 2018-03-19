85ccb6e2d960f87774ee39eb09f8fa5f56c739e0
alexdel <verbalab@yandex.ru>
2018-03-19 13:17:19 +0700

    Importing from vk api tool

--- data/overhear/scraper.py
+++ data/overhear/scraper.py
@@ -1,10 +1,32 @@
 import vk
-import os
-import json
-
+import pandas as pd
+import re
+from time import sleep
 
 session = vk.Session(access_token='')
 vk_api = vk.API(session)
-obj = vk_api.wall.search(owner_id=-34215577, offset=100, owners_only=True, count=100, query='#Подслушано_БЕСИТ@overhear', version='5.69')
-print(obj[0])
-#print(len(obj))
+
+results = []
+offset = 0
+
+def stripTags(text):
+    return re.sub('<[^<]+?>', '', text)
+
+def makeApiRequest(offset, query, label=''):
+    response = vk_api.wall.search(owner_id=-34215577, offset=offset, owners_only=True, count=100, query=query, version='5.69')
+    return (response[0], [{"text":stripTags(item['text'].replace(query, '')), "label": label } for item in response[1:]])
+
+tagsEmotionsMapping = [
+    ('#Подслушано_БЕСИТ@overhear', 'anger')
+]
+
+count = makeApiRequest(0, '#Подслушано_БЕСИТ@overhear')[0]
+
+while len(results) < count:
+    sleep(2)
+    (count_, results_) = makeApiRequest(offset, '#Подслушано_БЕСИТ@overhear', 'anger')
+    results.extend(results_)
+    offset += 100
+
+df = pd.DataFrame(results)
+df.to_csv('anger.csv', encoding='utf-8')
