6c0790faa156324c83dd74d288a9988f22f2e55f
alexdel <verbalab@yandex.ru>
2018-03-20 13:28:10 +0700

    Scraper enhanced.

--- data/overhear/scraper.py
+++ data/overhear/scraper.py
@@ -6,27 +6,38 @@ from time import sleep
 session = vk.Session(access_token='')
 vk_api = vk.API(session)
 
-results = []
-offset = 0
-
-def stripTags(text):
-    return re.sub('<[^<]+?>', '', text)
-
-def makeApiRequest(offset, query, label=''):
-    response = vk_api.wall.search(owner_id=-34215577, offset=offset, owners_only=True, count=100, query=query, version='5.69')
-    return (response[0], [{"text":stripTags(item['text'].replace(query, '')), "label": label } for item in response[1:]])
-
 tagsEmotionsMapping = [
-    ('#Подслушано_БЕСИТ@overhear', 'anger')
+    ('#Подслушано_БЕСИТ@overhear', 'anger'),
+    ('#Подслушано_успех@overhear', 'excitement'),
+    ('#Подслушано_одиночество@overhear', 'distress'),
+    ('#Подслушано_наблюдения@overhear', 'startle'),
+    ('#Подслушано_страшное@overhear', 'fear'),
+    ('#Подслушано_счастье@overhear', 'enjoyment'),
+    ('#Подслушано_стыдно@overhear', 'shame'),
+    ('#Подслушано_фууу@overhear', 'disgust'),
 ]
 
-count = makeApiRequest(0, '#Подслушано_БЕСИТ@overhear')[0]
+for query, emotion in tagsEmotionsMapping:
+    print('Processing started: ' + query)
+    results = []
+    offset = 0
 
-while len(results) < count:
-    sleep(2)
-    (count_, results_) = makeApiRequest(offset, '#Подслушано_БЕСИТ@overhear', 'anger')
-    results.extend(results_)
-    offset += 100
+    def stripTags(text):
+        return re.sub('<[^<]+?>', '', text)
 
-df = pd.DataFrame(results)
-df.to_csv('anger.csv', encoding='utf-8')
+    def makeApiRequest(offset, query, emotion=''):
+        response = vk_api.wall.search(owner_id=-34215577, offset=offset, owners_only=True, count=100, query=query, version='5.69')
+        return (response[0], [{"text":stripTags(item['text'].replace(query, '')), "emotion": emotion, "source_type": "vk", "source_name": "overhear" } for item in response[1:]])
+
+    count = makeApiRequest(0, query)[0]
+
+    while len(results) < count:
+        sleep(2)
+        (count_, results_) = makeApiRequest(offset, query, emotion)
+        results.extend(results_)
+        offset += 100
+
+    df = pd.DataFrame(results)
+    df.to_csv('anger.csv', encoding='utf-8')
+
+print('All done')
