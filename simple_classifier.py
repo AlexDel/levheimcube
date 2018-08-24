9adc89a944dbdab323d11455be1933a82373549c
alexdel <verbalab@yandex.ru>
2018-08-24 13:03:18 +0700

    SVC classifier added.

--- simple_classifier.py
+++ simple_classifier.py
@@ -1,10 +1,15 @@
-import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
 from data import vkDataFrame
 from features import CountFearFeatures_body, CountFearFeatures_time
 
 vkDataFrame['body_ratio'] = vkDataFrame['text'].apply(CountFearFeatures_body)
 vkDataFrame['time_ratio'] = vkDataFrame['text'].apply(CountFearFeatures_time)
+
+#Some data and statistics
 
 print('Структура таблицы:')
 print(vkDataFrame.info())
@@ -19,3 +24,21 @@ print(vkGrouped['body_ratio'].describe())
 
 print('Статистика по времени:')
 print(vkGrouped['time_ratio'].describe())
+
+#Clasifier itslef#
+X = vkDataFrame.loc[:, ['body_ratio', 'time_ratio']].values
+
+labelEncoder = preprocessing.LabelEncoder()
+y = labelEncoder\
+    .fit(vkDataFrame['emotion'].unique())\
+    .transform(vkDataFrame['emotion'].values)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+classifier = LinearSVC()
+classifier.fit(X_train, y_train)
+
+y_predictions = classifier.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_predictions)
+print('Average precision-recall score: {0}'.format(accuracy))
