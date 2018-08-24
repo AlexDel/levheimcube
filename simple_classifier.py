519df699bc3e115bf36db2d76162a5fe8733769b
alexdel <verbalab@yandex.ru>
2018-08-24 12:11:22 +0700

    Grouping by emotions.

--- simple_classifier.py
+++ simple_classifier.py
@@ -4,12 +4,18 @@ from data import vkDataFrame
 from features import CountFearFeatures_body, CountFearFeatures_time
 
 vkDataFrame['body_ratio'] = vkDataFrame['text'].apply(CountFearFeatures_body)
+vkDataFrame['time_ratio'] = vkDataFrame['text'].apply(CountFearFeatures_time)
 
 print('Структура таблицы:')
 print(vkDataFrame.info())
 
 print('Первые 5 элементов:')
-print(vkDataFrame.loc[:, ['text', 'body_ratio', 'emotion']].head())
+print(vkDataFrame.loc[:, ['text', 'body_ratio', 'time_ratio', 'emotion']].head())
+
+vkGrouped = vkDataFrame.groupby('emotion')
 
 print('Статистика по частям тела:')
-print(vkDataFrame['body_ratio'].describe())
+print(vkGrouped['body_ratio'].describe())
+
+print('Статистика по времени:')
+print(vkGrouped['time_ratio'].describe())
