b0b2ef9dcfba8c2eaa35dd2c5bb0cf6a83994b6a
alexdel <verbalab@yandex.ru>
2018-08-24 10:02:34 +0700

    Some fixes.

--- data/overhear/loader.py
+++ data/overhear/loader.py
@@ -4,5 +4,5 @@ import pandas as pd
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '*.csv')
 
-vkDataFrame = pd.concat([pd.read_csv(f, index_col=0) for f in glob.glob(path)], ignore_index = True).dropna(how='any')
+vkDataFrame = pd.concat([pd.read_csv(f, index_col=0, encoding='utf-8') for f in glob.glob(path)], ignore_index = True).dropna(how='any')
 
