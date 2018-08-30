8fab8e73e1b359b110ebd7c97f5671a9af9a1e1d
alexdel <verbalab@yandex.ru>
2018-08-30 12:29:02 +0700

    Add normalized text as string

--- data/overhear/loader.py
+++ data/overhear/loader.py
@@ -7,24 +7,25 @@ from tools.analizer import normalize
 
 sys._enablelegacywindowsfsencoding()
 
-currentDir = os.path.dirname(os.path.abspath(__file__))
-path = os.path.join(currentDir, '*.csv')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(current_dir, '*.csv')
 
 def getVkData(force_reload = True):
-    picklePath = os.path.join(currentDir, 'vk.pkl')
-    pickleExist = os.path.isfile(picklePath)
+    pickle_path = os.path.join(current_dir, 'vk.pkl')
+    pickle_exist = os.path.isfile(pickle_path)
 
-    if not force_reload and pickleExist:
-        return pd.read_pickle(picklePath)
+    if not force_reload and pickle_exist:
+        return pd.read_pickle(pickle_path)
     else:
-        if pickleExist:
-            os.remove(picklePath)
+        if pickle_exist:
+            os.remove(pickle_path)
 
         vkDataFrame = pd.concat([pd.read_csv(f, index_col=0, encoding='utf-8') for f in glob.glob(path)],
                                 ignore_index=True).dropna(how='any')
 
         vkDataFrame['normalized_text'] = vkDataFrame['text'].apply(normalize)
-        vkDataFrame.to_pickle(picklePath)
+        vkDataFrame['normalized_text_as_string'] = vkDataFrame['normalized_text'].apply(lambda word_list: ' '.join(word_list))
+        vkDataFrame.to_pickle(pickle_path)
 
         return vkDataFrame
 
