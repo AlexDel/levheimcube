616ba8692cbda0dc756fa7de2eacc3aec16bb1f1
alexdel <verbalab@yandex.ru>
2018-09-03 12:48:17 +0700

    Add pipeline for classifier

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,8 +1,13 @@
+import numpy as np
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
 
 from data import getVkData
 
@@ -32,9 +37,16 @@ count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
 X_test_counts = count_vect.transform(X_test)
 
-clf = MultinomialNB().fit(X_train_counts,  y_train)
+nb_pipeline = make_pipeline(MultinomialNB())
+nb_parameters = dict(multinomialnb__alpha=np.linspace(0.8, 1.0, num=5),
+                     multinomialnb__fit_prior=[True, False])
+cv = StratifiedKFold(n_splits=10)
 
-y_predictions = clf.predict(X_test_counts)
+model = GridSearchCV(nb_pipeline, param_grid=nb_parameters, scoring='precision_weighted', cv=cv)
+
+model.fit(X_train_counts,  y_train)
+
+y_predictions = model.predict(X_test_counts)
 
 report = classification_report(y_test, y_predictions, target_names=labelEncoder.classes_)
 print(report)
