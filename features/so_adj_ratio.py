953861cad79ff2fbe1626ff7e855c0d5b1ec4897
AlexDel <verbalab@yandex.ru>
2018-11-20 06:23:55 +0700

    Changed feature function body to work with new structure.

--- features/so_adj_ratio.py
+++ features/so_adj_ratio.py
@@ -1,21 +1,20 @@
 import pymorphy2
 
-def CountStartleFeatures_SoAdj(tokens):
-    words = text.split()
+def CountStartleFeatures_SoAdj(tokens_parsed=[]):
     soTokens = ['такой', 'так']
 
     counter = 0
-    morph = pymorphy2.MorphAnalyzer()
 
-    if len(words) == 0:
+    if len(tokens_parsed) == 0:
         return 0
-    for i in range(len(words)-1):
-        if words[i].strip() in soTokens:
-            p = morph.parse(words[i+1])[0]
-            tag = p.tag.POS
-            if tag == 'ADJF' or tag == 'ADJS':
+
+    for i, t in enumerate(tokens_parsed):
+        current_token = tokens_parsed[i][0]
+        next_token_pos_tag = tokens_parsed[i+1][1]
+        if current_token in soTokens and next_token_pos_tag in ('ADJF', 'ADJS'):
                 counter += 1
-    return counter / (len(words) - 1)
+
+    return counter / len(tokens_parsed)
 
 #uncomment to debug
 #text = open('../data/overhear/startle.csv').read()
