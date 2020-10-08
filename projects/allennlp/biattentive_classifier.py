c8221e1f559c5f85ca37833d32fb9fb1379db32b
akalinin <akalinin@issart.com>
2020-10-08 11:23:40 +0700

    Biattentive classifier started WIP

--- projects/allennlp/biattentive_classifier.py
+++ projects/allennlp/biattentive_classifier.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 import torch
 from allennlp.models import Model
 from allennlp.data import Vocabulary
@@ -7,6 +5,7 @@ from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import Embedding
 from allennlp.modules import FeedForward
 from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
+from allennlp.common.checks import check_dimensions_match
 
 
 EMBEDDING_DIM = 512
@@ -29,14 +28,23 @@ class BiattentiveClassifier(Model):
 
         self._num_classes = self.vocab.get_vocab_size('labels')
 
-        self._pre_encode_feed_forward = FeedForward(input_dim=EMBEDDING_DIM, num_layers=1, activations=torch.nn.ReLU, hidden_dims=[EMBEDDING_DIM])
+        self._pre_encode_feed_forward = FeedForward(input_dim=EMBEDDING_DIM, num_layers=1, activations=torch.nn.ReLU, hidden_dims=[HIDDEN_DIM])
         self._encoder  = PytorchSeq2SeqWrapper(torch.nn.LSTM(EMBEDDING_DIM, HIDDEN_DIM, batch_first=True))
 
-        self._integrator = PytorchSeq2SeqWrapper(torch.nn.LSTM(EMBEDDING_DIM, HIDDEN_DIM, batch_first=True))
+        self._integrator = PytorchSeq2SeqWrapper(torch.nn.LSTM(EMBEDDING_DIM * 2, HIDDEN_DIM, batch_first=True))
         self._integrator_dropout = torch.nn.Dropout(EMBEDDING_DROPOUT)
         self._combined_integrator_output_dim = self._integrator.get_output_dim()
 
+        self._self_attentive_pooling_projection = torch.nn.Linear(self._combined_integrator_output_dim, 1)
 
+        self._output_layer = FeedForward(input_dim=HIDDEN_DIM * 4, num_layers=1, hidden_dims=self._num_classes, activations=torch.nn.ReLU)
+
+        check_dimensions_match(
+            self._text_field_embedder.get_output_dim(),
+            self._pre_encode_feed_forward.get_input_dim(),
+            "text field embedder output dim",
+            "Pre-encoder feedforward input dim",
+        )
 
 
 
