da415f6750209598137476f8ca45b49fd55c8dd4
alexdel <akalinin@issart.com>
2021-11-12 11:54:50 +0700

    Prediction fixed

--- app/use_cases.py
+++ app/use_cases.py
@@ -1,7 +1,7 @@
 import torch
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoTokenizer
 
-from projects.regression.regression_lightning import BERT, RegressionBert
+from projects.regression.regression_lightning import BERT,MAX_LENGTH, RegressionBert
 
 
 CHECKPOINT = 'https://storage.yandexcloud.net/nlp-dataset-bucket-1/models/regression-bert-val_loss%3D1.8566.ckpt'
@@ -13,9 +13,6 @@ def predict_diagonal_values(text):
     tokens = tokenizer.encode(text, padding='max_length', add_special_tokens=True, max_length=MAX_LENGTH, truncation=True)
     tokens = torch.tensor(tokens)
 
-    prediction = model(tokens)
+    prediction = model(torch.unsqueeze(tokens, 0))
 
     return prediction
-
-
-#p = predict_diagonal_values('Работаю педиатром в инфекционном отделении. Поступает к нам мальчик с гастроэнтеритом (диарея, рвота). Один из главных вопросов: "Что ребёнок ел?" Мама: "Сейчас мы болеем, так что только грудь". Ребёнку 6 лет.')
