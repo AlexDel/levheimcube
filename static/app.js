6d75a67a97ff35bb98e0b82aeb55cf76c815eb1f
AlexDel <verbalab@yandex.ru>
2021-12-26 20:09:17 +0300

    WIP

--- static/app.js
+++ static/app.js
@@ -100,6 +100,9 @@ function App() {
     >
       <div style={{maxWidth: 700, margin: '0 auto'}}>
         <Paper>
+         <Typography variant="body2" gutterBottom>
+               The web application has been developed within a project No. 19-012-00205 "Design of sentiment classifier for Internet-texts in Russian backed by Lövheim\'s Cube emotional model" supported by the Russian Foundation for Basic Research and carried out by the researcher team of the Laboratory of Applied Linguistics and Cognitive Research (School of Philology and Language Communication) of the Siberian Federal University.
+          </Typography>
           <TextField
             id="standard-multiline-static"
             label="Enter the text (Only Russian is supported)"
